b3f9781b50197df9827859b6a8ec316c3d03bb9d
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-01-24 12:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const byteSize = (str) => {
-  // write your code here
+  // Create a Blob object with the given string
+  const blob = new Blob([str]);
+
+  // Get the size property of the Blob object, which represents the size in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// Test cases
+const size1 = byteSize('hello world');      // Output: 11
+const size2 = byteSize('안녕하세요');           // Output: 15
+const size3 = byteSize('');                  // Output: 0
+
+console.log(size1);
+console.log(size2);
+console.log(size3);
